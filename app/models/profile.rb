e819edf4940b560ee4b18089b631cd2d3c0bf2b3
AnnushkaV <verezub_anna@mail.ru>
2015-06-15 10:42:28 +0300

    add default img

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/default_avatar.png"
+  has_attached_file :avatar,
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => ActionController::Base.helpers.asset_path('avatar_missing.png')
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   delegate :name, :name=, :email, :email=, :to => :user
